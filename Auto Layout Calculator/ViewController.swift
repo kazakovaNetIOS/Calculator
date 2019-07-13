444f1ef20449fa145a99fd47e6d2ac0f13432304
nkazakova <natalia.kazakova@innoway.ru>
2019-07-13 17:07:32 +0300

    Version 1

--- Auto Layout Calculator/ViewController.swift
+++ Auto Layout Calculator/ViewController.swift
@@ -9,14 +9,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func buttonPressed(_ sender: UIButton) {
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    private var calculator = CalculatorLogic()
+    private var isFinishedTypingNumber: Bool = true
+    
+    private var displayValue: Double {
+        get {
+            guard let number = Double(displayLabel.text!) else {
+                fatalError("Cannot convert display label text to a Double")
+            }
+            return number
+        }
+        set {
+            displayLabel.text = String(newValue)
+        }
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        displayLabel.text = "0"
+    }
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        
+        calculator.setNumber(displayValue)
+        
+        if let calcMethod = sender.currentTitle {
+            if let result = calculator.calculate(symbol: calcMethod) {
+                displayValue = result
+            }            
+        }
+    }
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                if numValue == "." {
+                    let isInt = floor(displayValue) == displayValue
+                    
+                    if !isInt { return }
+                }
+                
+                displayLabel.text?.append(numValue)
+            }
+        }
+    }
 }
 
